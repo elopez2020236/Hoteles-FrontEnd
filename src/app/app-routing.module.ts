ca500488044b3f9b20b1e43544aafa22d86063ac
Edwar Esaul López Pérez <88805623+elopez2020236@users.noreply.github.com>
2022-06-29 22:45:14 -0600

    Transpaso de Archivos

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DetalleHabitacionComponent } from './components/detalle-habitacion/detalle-habitacion.component';
+import { HabitacionesComponent } from './components/habitaciones/habitaciones.component';
+import { HomeComponent } from './components/home/home.component';
+import { HotelesComponent } from './components/hoteles/hoteles.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'hoteles', component: HotelesComponent },
+  { path: 'habitaciones', component: HabitacionesComponent},
+  { path: 'detalleHabitacion/:idHabitacion', component: DetalleHabitacionComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { HomeComponent } from './components/home/home.component';
+import { FormsModule } from '@angular/forms';
+import { HotelesComponent } from './components/hoteles/hoteles.component';
+import { HabitacionesComponent } from './components/habitaciones/habitaciones.component';
+import { DetalleHabitacionComponent } from './components/detalle-habitacion/detalle-habitacion.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    SignInComponent,
+    SignUpComponent,
+    HomeComponent,
+    HotelesComponent,
+    HabitacionesComponent,
+    DetalleHabitacionComponent
+  ],
+
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+  ],
+
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

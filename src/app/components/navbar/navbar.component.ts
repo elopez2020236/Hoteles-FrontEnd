ba8e81c9d102ab8f224242072331e25cbca60ca8
Edwar Esaul López Pérez <88805623+elopez2020236@users.noreply.github.com>
2022-07-02 19:21:23 -0600

    Cerrar Sesion

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  providers: [UsuarioService]
+
 })
 export class NavbarComponent implements OnInit {
+    public token: any;
 
-  constructor() { }
+    constructor(
+      public _usuarioService: UsuarioService) {
+        this.token = this._usuarioService.getToken();
 
+     }
   ngOnInit(): void {
   }
 
+  VaciarToken(){
+    this._usuarioService.clearToken();
+    Swal.fire({
+      icon: 'success',
+      title: 'Sesión Cerrada',
+      showConfirmButton: false,
+      timer: 1500
+    })
+  }
 }

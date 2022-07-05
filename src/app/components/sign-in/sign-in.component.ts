0f24eb691a9f634420d8265710b755a89092ce95
Genrry Uwualdo Luis Alvarado <88805705+gluis2020232@users.noreply.github.com>
2022-07-04 22:20:17 -0600

    Validación Navar

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -7,7 +7,6 @@ import Swal from 'sweetalert2';
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.scss'],
-  providers: [UsuarioService],
 })
 export class SignInComponent implements OnInit {
   public userModel: Usuario;
@@ -21,17 +20,6 @@ export class SignInComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  getToken() {
-    this._usuarioService.login(this.userModel, 'true').subscribe(
-      (response) => {
-        console.log(response.token);
-        localStorage.setItem('token', response.token);
-      },
-      (error) => {
-        console.log(<any>error);
-      }
-    );
-  }
   getTokenPromesa(): Promise<any> {
     return new Promise((resolve, reject) => {
       this._usuarioService.login(this.userModel, 'true').subscribe(
@@ -45,11 +33,11 @@ export class SignInComponent implements OnInit {
       );
     });
   }
+
   login() {
     this._usuarioService.login(this.userModel).subscribe(
       (response) => {
         this.getTokenPromesa().then((respuesta) => {
-          console.log(respuesta);
           localStorage.setItem('identidad', JSON.stringify(response.usuario));
 
           this._router.navigate(['/hoteles']);

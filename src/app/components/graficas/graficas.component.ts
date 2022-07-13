9b3cd91f161ea9872c5085383fa1f2c5b0452de9
Genrry Uwualdo Luis Alvarado <88805705+gluis2020232@users.noreply.github.com>
2022-07-13 11:16:16 -0600

    Agregando Funcionalidad Gráfica

--- src/app/components/graficas/graficas.component.ts
+++ src/app/components/graficas/graficas.component.ts
@@ -1,31 +1,65 @@
 import { Component, OnInit } from '@angular/core';
 import { HotelService } from 'src/app/services/hotel.service';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-graficas',
   templateUrl: './graficas.component.html',
   styleUrls: ['./graficas.component.scss'],
-  providers: [HotelService]
+  providers: [HotelService, UsuarioService]
 })
 export class GraficasComponent implements OnInit {
 
   chartOptions = {
     responsive: true,
   };
-  chartLabels = ['Download', 'Sales','Mail Sales'];
-  chartData = [300, 500, 100];
-  chartColors = [{
-    backgroundColor: ['red', '#0F0', 'rgba(41, 182, 246,0.75)'],
-    borderColor: ['rgb(250,120,100)', 'green', '#0086c3']
-  }];
+
+  //'Download', 'Sales','Mail Sales'
+
+  //Nombre Hoteles
+  chartLabels:any = [];
+
+  //Cantidas de hoteles
+  chartData:any = [300, 500, 100];
+
+  chartColors:any = [
+    {
+    backgroundColor: [],
+  }
+];
+
   chartLegend = true;
   chartPlugins = [];
 
-  constructor() { }
+  public modelHotelesGet: any = [];
+
+  constructor(
+    public _hotelService: HotelService,
+    public _usuarioService: UsuarioService
+  ) { }
 
   ngOnInit(): void {
+    this.getHoteles()
   }
 
- 
+  getHoteles(){
+    this._hotelService.obtenerHoteles(this._usuarioService.getToken()).subscribe(
+      (response) => {
+        console.log(response.hotel);
+        this.modelHotelesGet = response.hotel;
+        this.modelHotelesGet.forEach(dato => {
+          console.log(dato);
+          this.chartLabels.push(dato.nombre);
+          // this.chartData.push(dato.direccion);
+          // this.chartData.push(dato.telefono);
+          this.chartColors[0].backgroundColor.push(`#${ Math.floor(Math.random()*16777215).toString(16) }`);
+        })
+
+      },
+      (error) => {
+        console.log(error)
+      }
+    )
+  }
 
 }
